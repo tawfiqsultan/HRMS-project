1820c7a495b9e95c6d8cafac2c65902e037003b3
Tawfiq alqaidy <talqaidy@gmail.com>
2025-03-28 17:32:29 +0200

    Add payroll router for API endpoints

---
+++ HRMS-api/routes/api/payrollRouter.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PayrollController;
+
+Route::prefix('payroll')->group(function () {
+    Route::post('/generate', [PayrollController::class, 'generatePayroll']);
+    Route::get('/all', [PayrollController::class, 'getAllPayroll']);
+    Route::get('/payroll/{id}', [PayrollController::class, 'getPayroll']);
+    Route::get('/employee/{id}', [PayrollController::class, 'getEmployeePayrolls']);
+    Route::patch('/update/{id}', [PayrollController::class, 'updatePayroll']);
+});
