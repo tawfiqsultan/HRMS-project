4196d9331ce7c62c29d6def2342f29267b4639ad
Tawfiq alqaidy <talqaidy@gmail.com>
2025-03-30 14:44:42 +0200

    Add Laravel Sanctum for API authentication and configure middleware

--- HRMS-api/bootstrap/app.php
+++ HRMS-api/bootstrap/app.php
@@ -3,16 +3,17 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful; // add this line
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        api: __DIR__ . '/../routes/api.php',
+        api: __DIR__ . '/../routes/api.php', // تأكد أن هذا السطر موجود
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(EnsureFrontendRequestsAreStateful::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
